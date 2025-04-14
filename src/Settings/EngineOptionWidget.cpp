7b7a8b42a33bd0a8c328331bae7c910489dd2d34
KimYubin <yubink92@gmail.com>
2025-04-15 01:30:42 +0900

    [추가] 엔진 옵션 위젯 기본 추가
    1. 엔진 옵션을 위한 위젯 기본 베이스 추가

---
+++ src/Settings/EngineOptionWidget.cpp
@@ -0,0 +1,42 @@
+﻿//
+// Created by YubinKim on 25/04/14 월.
+//
+#include <QString>
+
+#include "EngineOptionWidget.h"
+#include "../../ui/ui_EngineOptionWidget.h"
+
+EngineOptionWidget::EngineOptionWidget()
+{
+}
+
+EngineOptionWidget::~EngineOptionWidget() {
+}
+
+void EngineOptionWidget::apply()
+{
+    IOptionWidget::apply();
+}
+
+void EngineOptionWidget::cancel()
+{
+    IOptionWidget::cancel();
+}
+
+void EngineOptionWidget::finish()
+{
+    IOptionWidget::finish();
+}
+
+EngineOption::EngineOption()
+{
+    setDisplayName(tr(""));
+    setIconPath(tr(""));
+    setOptionWidgetCtor([]() { return new EngineOptionWidget(); });
+}
+
+EngineOption::~EngineOption()
+{
+}
+
+const EngineOption engineOption;
---
+++ src/Settings/EngineOptionWidget.h
@@ -0,0 +1,43 @@
+﻿//
+// Created by YubinKim on 25/04/14 월.
+//
+
+#ifndef ENGINEOPTIONWIDGET_H
+#define ENGINEOPTIONWIDGET_H
+#include "IOptionWidget.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui
+{
+class SettingsWidget;
+}
+QT_END_NAMESPACE
+
+class EngineOptionWidget : public IOptionWidget
+{
+    Q_OBJECT
+
+public:
+    EngineOptionWidget();
+    ~EngineOptionWidget() override;
+
+protected:
+    friend class EngineOption;
+    virtual void apply() override;
+    virtual void cancel() override;
+    virtual void finish() override;
+};
+
+class EngineOption : public IOptionPage
+{
+    Q_DISABLE_COPY_MOVE(EngineOption)
+
+public:
+    EngineOption();
+    virtual ~EngineOption();
+
+    Ui::SettingsWidget* ui;
+};
+
+
+#endif //ENGINEOPTIONWIDGET_H

d966e6a7c5b71961485d37ad00089481348e00fb
KimYubin <yubink92@gmail.com>
2025-05-15 19:51:05 +0900

    [리펙] common->general 옵션으로 이름 변경

---
+++ src/Settings/GeneralOptionWidget.cpp
@@ -0,0 +1,56 @@
+﻿//
+// Created by YubinKim on 25/05/13 화.
+//
+
+// You may need to build the project (run Qt uic code generator) to get "ui_GeneralOptionWidget.h" resolved
+
+#include "GeneralOptionWidget.h"
+#include "ui_GeneralOptionWidget.h"
+
+
+GeneralOptionWidget::GeneralOptionWidget(QWidget* parent)
+    : IOptionWidget(parent)
+    , ui(new Ui::GeneralOptionWidget)
+{
+    ui->setupUi(this);
+    setLayout(ui->mainLayout);
+
+    
+}
+
+GeneralOptionWidget::~GeneralOptionWidget()
+{
+    delete ui;
+}
+
+
+void GeneralOptionWidget::apply()
+{
+    IOptionWidget::apply();
+}
+
+void GeneralOptionWidget::cancel()
+{
+    IOptionWidget::cancel();
+}
+
+void GeneralOptionWidget::finish()
+{
+    IOptionWidget::finish();
+}
+
+
+// ~======================
+// GeneralOption 
+GeneralOption::GeneralOption()
+{
+    setDisplayName(tr("일반"));
+    setIconPath(tr(""));
+    setOptionWidgetCtor([]() { return new GeneralOptionWidget(); });
+}
+
+GeneralOption::~GeneralOption()
+{
+}
+
+const GeneralOption engineOption;
---
+++ src/Settings/GeneralOptionWidget.h
@@ -0,0 +1,53 @@
+﻿//
+// Created by YubinKim on 25/05/13 화.
+//
+
+#ifndef GeneralOptionWIDGET_H
+#define GeneralOptionWIDGET_H
+
+#include <QWidget>
+
+#include "IOptionWidget.h"
+
+
+QT_BEGIN_NAMESPACE
+
+namespace Ui
+{
+class GeneralOptionWidget;
+}
+
+QT_END_NAMESPACE
+
+class GeneralOptionWidget : public IOptionWidget
+{
+    Q_OBJECT
+
+public:
+    explicit GeneralOptionWidget(QWidget* parent = nullptr);
+    ~GeneralOptionWidget() override;
+
+protected:
+    
+    virtual void apply() override;
+    virtual void cancel() override;
+    virtual void finish() override;
+
+private:
+    friend class GeneralOption;
+    Ui::GeneralOptionWidget* ui;
+};
+
+
+class GeneralOption : public IOptionPage
+{
+    Q_DISABLE_COPY_MOVE(GeneralOption)
+
+public:
+    GeneralOption();
+    virtual ~GeneralOption() override;
+
+};
+
+
+#endif //GeneralOptionWIDGET_H

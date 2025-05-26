ac284d8d745f0d84b0fc5585daa88ad2bdc3d53e
KimYubin <yubink92@gmail.com>
2025-05-26 18:22:42 +0900

    [개선] 셋팅 위젯 좌측 버튼 스크롤 추가. 좌측 버튼 정렬
    1. 셋팅 위젯 좌측 버튼 위쪽으로 붙여서 정렬 시켰음
    2. 스크롤 에리어도 추가함.
    3. 사이즈도 좌우로는 안늘어나게 개선함

--- src/Settings/SettingsWidget.cpp
+++ src/Settings/SettingsWidget.cpp
@@ -11,6 +11,7 @@
 #include <QPushButton>
 #include <QScrollBar>
 #include <QStyledItemDelegate>
+#include <QScrollArea>
 
 #include "IOptionWidget.h"
 
@@ -28,18 +29,30 @@ SettingsWidget::SettingsWidget(QWidget* parent)
 
     setAttribute(Qt::WA_DeleteOnClose);
 
-    ui->findEdit->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
-    ui->optionLabel->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+    ui->findEdit->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
+    ui->optionLabel->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
 
     // ~=====================
     // option setup
+    // scrollArea
+    QWidget* container       = new QWidget();
+    QVBoxLayout* innerLayout = new QVBoxLayout(container);
+    QScrollArea* scrollArea  = new QScrollArea(this);
+    scrollArea->setWidget(container);
+    scrollArea->setWidgetResizable(true);
+    scrollArea->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Preferred);
+    scrollArea->setAlignment(Qt::AlignTop);
+    scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarPolicy::ScrollBarAlwaysOff);
+    ui->buttonLayout->addWidget(scrollArea);
+
+
     _buttonGroup = new QButtonGroup(this);
     _buttonGroup->setExclusive(true);
 
     const std::vector<IOptionPage*> options = IOptionPage::sortedOptionsPages();
     for (IOptionPage* option : options)
     {
-        QPushButton* selectButton = new QPushButton(option->getIcon(), option->getDisplayName(), this);
+        QPushButton* selectButton = new QPushButton(option->getIcon(), option->getDisplayName(), container);
         selectButton->setProperty("selectButton", true);
         selectButton->setCheckable(true);
         selectButton->setFocusPolicy(Qt::TabFocus);
@@ -47,9 +60,13 @@ SettingsWidget::SettingsWidget(QWidget* parent)
 
         const int stkIdx = ui->optionStackedWidget->addWidget(option->getOptionWidget());
         _buttonGroup->addButton(selectButton, stkIdx);
-        ui->buttonLayout->addWidget(selectButton);
-        ui->buttonLayout->setAlignment(selectButton, Qt::AlignTop);
+        innerLayout->addWidget(selectButton);
+        innerLayout->setAlignment(selectButton, Qt::AlignTop);
     }
+
+    // 맨 위 쪽으로 정렬하기 위해 하단에 Spacer 추가
+    innerLayout->addStretch(1);
+
     connect(_buttonGroup, &QButtonGroup::idClicked, this, [this](const int inButtonId)
     {
         ui->optionStackedWidget->setCurrentIndex(inButtonId);

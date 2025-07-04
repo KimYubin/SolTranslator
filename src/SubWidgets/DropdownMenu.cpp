ffcddb538f80efe5614e7ad1f00bea96c8329a7d
KimYubin <yubink92@gmail.com>
2025-07-05 04:00:20 +0900

    [추가] 로컬라이제이션(번역) 기능 활성화및 파일 추가. dropdown메뉴 최초 커밋
    1. 로컬라이제이션용 기능들 활성화하고 파일 추가함. cmake로 빌드과정에서 자동으로 생성되게 만듦
    
    2. dropdown메뉴 최초 커밋. cmake 변경하면서 같이 커밋함

---
+++ src/SubWidgets/DropdownMenu.cpp
@@ -0,0 +1,10 @@
+﻿//
+// Created by YubinKim on 25/06/25 수.
+//
+
+#include "DropdownMenu.h"
+
+DropdownMenu::DropdownMenu(QWidget* parent): QComboBox(parent)
+{
+        
+}
---
+++ src/SubWidgets/DropdownMenu.h
@@ -0,0 +1,28 @@
+﻿//
+// Created by YubinKim on 25/06/25 수.
+//
+
+#ifndef DROPDOWNMENU_H
+#define DROPDOWNMENU_H
+#include <QComboBox>
+
+
+class DropdownMenu : public QComboBox
+{
+    Q_OBJECT
+
+public:
+    explicit DropdownMenu(QWidget* parent = nullptr);
+
+
+protected:
+
+
+private slots:
+
+
+private:
+};
+
+
+#endif //DROPDOWNMENU_H

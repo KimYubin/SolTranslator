e1fa51ff26edf1679066d4cd632544e07289c9b0
KimYubin <yubink92@gmail.com>
2025-07-15 20:13:07 +0900

    [추가] 커스텀 툴팁 추가
    1. 툴팁 모양 및 위치 제어를 위해 전용 툴팁 만듦
    
    2. 이벤트 필터 인스톨을 통해 사용 가능함.
    
    3. qss로 모양 변경 가능함

---
+++ src/Widgets/FinToolTip.cpp
@@ -0,0 +1,95 @@
+﻿//
+// Created by YubinKim on 25/07/15 화.
+//
+
+#include "FinToolTip.h"
+
+#include <QWidget>
+#include <QToolTip>
+
+#include <qevent.h>
+
+
+/** 커스텀 툴팁 말풍선 */
+class FinToolTipBallon : public QWidget
+{
+    Q_OBJECT
+
+public:
+    static FinToolTipBallon* instance()
+    {
+        static FinToolTipBallon* ins = new FinToolTipBallon();
+        return ins;
+    }
+    
+    explicit FinToolTipBallon(QWidget* parent = nullptr);
+
+    void showToolTip(const QString& inText, const QPoint& inPos);
+
+private:
+    QLabel* _label;
+};
+
+#include "FinToolTip.moc"
+
+
+
+FinToolTipBallon::FinToolTipBallon(QWidget* parent)
+    : QWidget(parent, Qt::ToolTip | Qt::FramelessWindowHint | Qt::NoDropShadowWindowHint)
+{
+    setObjectName("FinToolTipBallon");
+    setAttribute(Qt::WA_TransparentForMouseEvents);
+    setAttribute(Qt::WA_TranslucentBackground);
+    _label = new QLabel(this);
+}
+
+void FinToolTipBallon::showToolTip(const QString& inText, const QPoint& inPos)
+{
+    _label->setText(inText);
+    _label->adjustSize();
+    resize(_label->size());
+
+    QPoint newPos = {inPos.x() - (_label->width() / 2), inPos.y() - _label->height()};
+    move(newPos);
+    show();
+}
+
+
+// ~==================================
+// FinTooltipFilter 
+
+FinTooltipFilter::FinTooltipFilter(QObject* parent): QObject(parent)
+{
+}
+
+bool FinTooltipFilter::eventFilter(QObject* obj, QEvent* event)
+{
+    if (event->type() == QEvent::ToolTip)
+    {
+        const QHelpEvent* helpEvent = static_cast<QHelpEvent*>(event);
+        const QWidget* widget = qobject_cast<QWidget*>(obj);
+        if (widget == nullptr)
+        {
+            return false;
+        }
+
+        const QString tooltipText = widget->toolTip();
+        if (tooltipText.isEmpty() == false)
+        {
+            // 중앙 상단
+            const QSize widgetSize = widget->size();
+            const QPoint globalPos = widget->mapToGlobal(QPoint(0, 0)) + QPoint(0, -5);
+            const QPoint centerPos = {globalPos.x() + (widgetSize.width() / 2), globalPos.y()};
+
+            FinToolTipBallon::instance()->showToolTip(tooltipText, centerPos);
+
+            return true; // 기본 툴팁을 차단
+        }
+    }
+    else if (event->type() == QEvent::Leave)
+    {
+        FinToolTipBallon::instance()->hide();
+    }
+
+    return QObject::eventFilter(obj, event);
+}
---
+++ src/Widgets/FinToolTip.h
@@ -0,0 +1,31 @@
+﻿//
+// Created by YubinKim on 25/07/15 화.
+//
+
+#ifndef FINTOOLTIP_H
+#define FINTOOLTIP_H
+
+#include <QLabel>
+
+
+class FinToolTipBallon;
+
+/**
+ * 커스텀 툴팁을 표시하기 위한 이벤트 필터 객체입니다.
+ *
+ * widget->setToolTip("tool tip"));
+ * widget->installEventFilter(new FinTooltipFilter(qApp));
+ */
+class FinTooltipFilter : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit FinTooltipFilter(QObject *parent = nullptr);
+
+    virtual bool eventFilter(QObject *watched, QEvent *event) override;
+
+};
+
+
+#endif //FINTOOLTIP_H

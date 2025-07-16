8e0966905a117be643881c0a9c6e83c45bc68790
KimYubin <yubink92@gmail.com>
2025-07-16 19:12:42 +0900

    [개선] qt 현지화 적용 및 cmake 현지화 개선
    1. qt 번역을 적용할 수 있는 코드 추가
     - qt번역 적용한 번역 파일 생성 및 적용함
    
    2. cmake에 자동으로 현지화 작업 할 수 있도록 개선함
     - 잘 작동이 안되고 qt 번역이 적용 안됐던 문제를 수정함.
    
    3. 일본어가 jp가 아니라 ja 였음

--- localization/fin_ja.ts
+++ localization/fin_ja.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en" sourcelanguage="ko">
+<TS version="2.1" language="ja" sourcelanguage="ko">
 <context>
     <name>EngineOption</name>
     <message>

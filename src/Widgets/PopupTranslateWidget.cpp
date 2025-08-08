1207e9d1d3105ba0d379af6be38b3aefd265e37a
KimYubin <yubink92@gmail.com>
2025-08-09 04:16:40 +0900

    [리펙] 팝업번역 크기 관련 변수 주석 추가 및 기타 리펙 개선
    1. 팝업 크기 관련 변수에 주석
    
    2. yPosMax가 일반 상수였는데, 기반이 되는 maxSize와 수식으로 개선함
    
    3. 그외 기타
     - 타입을 배정밀도로 변경했음

--- src/Widgets/PopupTranslateWidget.cpp
+++ src/Widgets/PopupTranslateWidget.cpp
@@ -43,6 +43,11 @@ const QString codeBgColorStr = QString::fromLatin1("rgba(%1,%2,%3,%4)")
 
 PopupTranslateWidget::PopupTranslateWidget(QWidget* parent)
     : ITranslateWidget(parent, Qt::FramelessWindowHint | Qt::NoDropShadowWindowHint)
+    , _minSizeRatio(0.15, 0.15)
+    , _maxSizeRatio(0.2, 0.65)
+    , _fullSizeRatio(0.95, 0.95)
+    , _centerPosRatio(0.85, 0.33)
+    , _yPosMaxRatio((1.0 - _maxSizeRatio.height()) / 2.0)
     , ui(new Ui::PopupTranslateWidget)
 {
     QIcon icon = QIcon(":/img/icon_img");
@@ -257,11 +262,8 @@ void PopupTranslateWidget::setTextEditSize(const QSize& inTextEditSize)
     adjustSize();
 
     // 생성될 스크린 위치 추적
-    QPointF screenTopLeft  = QPointF();
     QScreen* currentScreen = nullptr;
-
-    const Fin::ScreenPopupPolicy screenPolicy = finConfig.getSimplePopupScreenPolicy();
-    switch (screenPolicy)
+    switch (finConfig.getSimplePopupScreenPolicy())
     {
     case Fin::ScreenPopupPolicy::Default:
     case Fin::ScreenPopupPolicy::PrimaryScreen:
@@ -277,11 +279,11 @@ void PopupTranslateWidget::setTextEditSize(const QSize& inTextEditSize)
         break;
     }
 
-    screenTopLeft = currentScreen ? currentScreen->geometry().topLeft() : QPointF();
+    const QPointF screenTopLeft = currentScreen ? currentScreen->geometry().topLeft() : QPointF();
 
     // position
     const QSizeF screenSize   = currentScreen ? currentScreen->size().toSizeF() : QSizeF(1920, 1080);
-    const QPoint targetCenter = screenTopLeft.toPoint() + QPoint(screenSize.width() * _centerPosRatio.x(), screenSize.height() * _centerPosRatio.y());
+    const QPoint targetCenter = screenTopLeft.toPoint() + QPointF(screenSize.width() * _centerPosRatio.x(), screenSize.height() * _centerPosRatio.y()).toPoint();
     const QPoint recCenter    = rect().center();
 
     QPoint targetPos = targetCenter - recCenter;
